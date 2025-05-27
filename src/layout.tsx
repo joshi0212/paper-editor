c6b72d808bc4dbac6d5f407aee0d9ad4d3115dd5
joshi0212 <105245096+joshi0212@users.noreply.github.com>
2025-05-27 12:25:58 +0000

    Styling in Import

--- src/layout.tsx
+++ src/layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 export default function Layout() {
-  return <div>
+  return <div className='overflow-hidden h-screen w-screen'>
       <Navbar />
       <Outlet />
     </div>
