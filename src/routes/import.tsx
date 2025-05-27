4a99135b865493d6d26b5f5603bf4ec8051715b5
joshi0212 <105245096+joshi0212@users.noreply.github.com>
2025-05-27 12:46:22 +0000

    button

--- src/routes/import.tsx
+++ src/routes/import.tsx
@@ -2,12 +2,15 @@ import clsx from "clsx";
 
 
 export default function Import() {
-    const divStyle = "flex h-full w-full border-black border-2";
+    const divStyle = "flex h-full w-full border-black border-2 p-4 ";
+    const buttonStyle = "w-32 h-12 border-black border-2 mx-auto rounded-md";
     return (
         
-        <div className="flex h-full justify-evenly">
+        <div className="flex h-full justify-evenly  ">
             <div className= {`${divStyle}`} >
-                test
+                <button className={buttonStyle}>
+                    + Notes
+                </button>
             </div>
                 <div className={`${divStyle}`} >
                 test
