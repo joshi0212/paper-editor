f6add027190c1ac60e7342c4c125e4676a6ffe94
joshi0212 <j.barkow1@gmail.com>
2025-05-27 14:55:17 +0200

    Remove DaisyUI from tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,38 +4,6 @@ export default {
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: [
-      
-    
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-    ],
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  
+  plugins: [require("@tailwindcss/typography")],
 };
