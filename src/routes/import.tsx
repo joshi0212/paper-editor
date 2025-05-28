0e3556027e587ccc26442232662b75371fd2fe4f
starlight_caffeine <liamstdm@gmail.com>
2025-05-28 20:12:35 +0200

    Mockup import page

--- src/routes/import.tsx
+++ src/routes/import.tsx
@@ -1,22 +1,26 @@
-import clsx from "clsx";
-
-
 export default function Import() {
     const divStyle = "flex h-full w-full border-black border-2 p-4 ";
     const buttonStyle = "w-32 h-12 border-black border-2 mx-auto rounded-md";
+    const blockStyle = "w-full h-10 rounded-md my-1 border-1 hover:bg-black hover:text-white";
     return (
-        
         <div className="flex h-full justify-evenly  ">
-            <div className= {`${divStyle}`} >
+            <div className={`${divStyle}`} >
                 <button className={buttonStyle}>
                     + Notes
                 </button>
             </div>
-                <div className={`${divStyle}`} >
-                test
+            <div className={`${divStyle}`} >
+                <button className={buttonStyle}>
+                    + Literature
+                </button>
             </div>
             <div className={`${divStyle}`}>
-                test
+                <div className="flex flex-col w-full">
+                    <button className={blockStyle}>Block 1</button>
+                    <button className={blockStyle}>Block 2</button>
+                    <button className={blockStyle} >Block 3</button>
+                    <button className={blockStyle}>Block 4</button>
+                </div>
             </div>
         </div>
     );
